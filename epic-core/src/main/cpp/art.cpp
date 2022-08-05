1174909d86c3b964007c3ea380204f40f892158f
sanbo <sanbo.xyz@gmail.com>
2022-08-05 17:10:21 +0800

    update sth. at 2022-08-05 17:10:21

--- epic-core/src/main/cpp/art.cpp
+++ epic-core/src/main/cpp/art.cpp
@@ -17,87 +17,88 @@
 #include <android/log.h>
 #include <cstddef>
 
-#define LOGV(...) ((void)__android_log_print(ANDROID_LOG_INFO, "epic.Native.art", __VA_ARGS__))
+#define TAG_NAME    "epic.art"
+#define LOGV(...)  ((void)__android_log_print(ANDROID_LOG_VERBOSE, TAG_NAME, __VA_ARGS__))
+#define LOGD(...)  ((void)__android_log_print(ANDROID_LOG_DEBUG, TAG_NAME, __VA_ARGS__))
+#define LOGI(...)  ((void)__android_log_print(ANDROID_LOG_INFO, TAG_NAME, __VA_ARGS__))
+#define LOGE(...)  ((void)__android_log_print(ANDROID_LOG_ERROR, TAG_NAME, __VA_ARGS__))
 
 #define ANDROID_R_API 30
 #define MAX_SEARCH_LEN 2000
 
-void* ArtHelper::runtime_instance_ = nullptr;
+void *ArtHelper::runtime_instance_ = nullptr;
 int ArtHelper::api = 0;
 
-template <typename T>
-inline int find_offset(void* hay, int size, T needle)
-{
-  for (int i = 0; i < size; i += sizeof(T)) {
-    auto current = reinterpret_cast<T*>(reinterpret_cast<char*>(hay) + i);
-    if (*current == needle) {
-      return i;
+template<typename T>
+inline int find_offset(void *hay, int size, T needle) {
+    for (int i = 0; i < size; i += sizeof(T)) {
+        auto current = reinterpret_cast<T *>(reinterpret_cast<char *>(hay) + i);
+        if (*current == needle) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
-void ArtHelper::init(JNIEnv* env, int api)
-{
-  ArtHelper::api = api;
-  JavaVM* javaVM;
-  env->GetJavaVM(&javaVM);
-  JavaVMExt* javaVMExt = (JavaVMExt*)javaVM;
+void ArtHelper::init(JNIEnv *env, int api) {
+    // 获取art的runtime
+    ArtHelper::api = api;
+    JavaVM *javaVM;
+    env->GetJavaVM(&javaVM);
+    JavaVMExt *javaVMExt = (JavaVMExt *) javaVM;
 
-  void* runtime = javaVMExt->runtime;
-  if (runtime == nullptr) {
-    return;
-  }
+    void *runtime = javaVMExt->runtime;
+    if (runtime == nullptr) {
+        return;
+    }
 
-  if (api < ANDROID_R_API) {
-    runtime_instance_ = runtime;
-  } else {
-    int vm_offset = find_offset(runtime, MAX_SEARCH_LEN, javaVM);
-    runtime_instance_ = reinterpret_cast<void*>(reinterpret_cast<char*>(runtime) + vm_offset - offsetof(PartialRuntimeR, java_vm_));
-  }
+    // 安卓10+版本计算偏移，最终获取runtime
+    if (api < ANDROID_R_API) {
+        runtime_instance_ = runtime;
+    } else {
+        int vm_offset = find_offset(runtime, MAX_SEARCH_LEN, javaVM);
+        runtime_instance_ = reinterpret_cast<void *>(reinterpret_cast<char *>(runtime) + vm_offset -
+                                                     offsetof(PartialRuntimeR, java_vm_));
+    }
 }
 
-void* ArtHelper::getClassLinker()
-{
-  if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
-    return nullptr;
-  }
-  PartialRuntimeR* runtimeR = (PartialRuntimeR*)runtime_instance_;
-  return runtimeR->class_linker_;
+void *ArtHelper::getClassLinker() {
+    if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
+        return nullptr;
+    }
+    PartialRuntimeR *runtimeR = (PartialRuntimeR *) runtime_instance_;
+    return runtimeR->class_linker_;
 }
 
-void* ArtHelper::getJniIdManager()
-{
-  if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
-    return nullptr;
-  }
-  PartialRuntimeR* runtimeR = (PartialRuntimeR*)runtime_instance_;
-  return runtimeR->jni_id_manager_;
+void *ArtHelper::getJniIdManager() {
+    if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
+        return nullptr;
+    }
+    PartialRuntimeR *runtimeR = (PartialRuntimeR *) runtime_instance_;
+    return runtimeR->jni_id_manager_;
 }
 
-void* ArtHelper::getJitCodeCache()
-{
-  if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
-    return nullptr;
-  }
-  PartialRuntimeR* runtimeR = (PartialRuntimeR*)runtime_instance_;
-  return runtimeR->jit_code_cache_;
+void *ArtHelper::getJitCodeCache() {
+    if (runtime_instance_ == nullptr || api < ANDROID_R_API) {
+        return nullptr;
+    }
+    PartialRuntimeR *runtimeR = (PartialRuntimeR *) runtime_instance_;
+    return runtimeR->jit_code_cache_;
 }
 
-void* ArtHelper::getHeap()
-{
-  if (runtime_instance_ == nullptr) {
-    return nullptr;
-  }
-  if (api < 26) {
-    Runtime_7X* runtime7X = (Runtime_7X*)runtime_instance_;
-    return runtime7X->heap_;
-  } else if (api < ANDROID_R_API) {
-    Runtime_8X* runtime8X = (Runtime_8X*)runtime_instance_;
-    LOGV("bootclasspath : %s", runtime8X->boot_class_path_string_.c_str());
-    return runtime8X->heap_;
-  } else {
-    PartialRuntimeR* runtimeR = (PartialRuntimeR*)runtime_instance_;
-    return runtimeR->heap_;
-  }
+void *ArtHelper::getHeap() {
+    if (runtime_instance_ == nullptr) {
+        return nullptr;
+    }
+    if (api < 26) {
+        Runtime_7X *runtime7X = (Runtime_7X *) runtime_instance_;
+        return runtime7X->heap_;
+    } else if (api < ANDROID_R_API) {
+        Runtime_8X *runtime8X = (Runtime_8X *) runtime_instance_;
+        LOGV("bootclasspath : %s", runtime8X->boot_class_path_string_.c_str());
+        return runtime8X->heap_;
+    } else {
+        PartialRuntimeR *runtimeR = (PartialRuntimeR *) runtime_instance_;
+        return runtimeR->heap_;
+    }
 }
